53fd0f16823d9bd936bad439a13da5ca003982a0
Jacob Lepler <lepler.jacob@gmail.com>
2020-12-29 20:46:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,6 +2,9 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
+  array.each do |el|
+    print "printed element: #{el}"
+  end
 end
 
 def list_dwarves(array)
